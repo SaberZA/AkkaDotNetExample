614e1f6310c0f20023347f14103a1f89c3c87f76
Steven van der Merwe <steven.vandermerwe@kaleidocode.co.za>
2015-08-13 17:04:59 +0200

    Initial Commit

---
+++ MovieStreaming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Actor.Internal;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem _movieStreamingActorSystem;
+        static void Main(string[] args)
+        {
+            _movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine(string.Format("{0} started...",_movieStreamingActorSystem.Name));
+            Console.ReadLine();
+
+            _movieStreamingActorSystem.Shutdown();
+        }
+    }
+}
